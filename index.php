afc97f26908191c443dc62131d21e07c95f3ffa7
DevCrew02 <63283868+DevCrew02@users.noreply.github.com>
2020-09-04 10:51:11 +0200

    splash working

--- index.php
+++ index.php
@@ -1,37 +1,35 @@
-<?php
-
-session_start();
-
-if (isset($_GET['msg'])) {
-    echo $_GET['msg'];
-}
-?>
-
-<!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=yes">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="HandheldFriendly" content="true">
-    <link rel="stylesheet" href="css/main-style.css">
-    <link rel="stylesheet" href="splash-screen.css">
-    <title>Space miner</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="HandheldFriendly" content="true">
+<meta http-equiv = "refresh" content = "3; url = index2.php" />
+<title>PHP 2020 Game</title>
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js"></script>
+<style type="text/css">
+#splash {
+    position:absolute;
+    top:0;
+    left:0;
+    bottom:0;
+	height:100%;
+    width:100%;
+    background-color:black;
+}
+</style>
+ 
+ 
+ 
+<script type="text/javascript">  
+$(function(){
+   setTimeout(function() {
+      $('#splash').fadeOut(500);
+   }, 2000);
+});
+</script>
+ 
 </head>
 <body>
-    <h1>Welcome to Space miner!</h1>
-    <br>
-    <div class="form">
-    <h1>Space miner</h1>
-    <p>Login to start playing the game!</p>
-    <a href="login.php">Login</a>
-    <br>
-    <br>
-    <br>
-    <hr>
-    <br>
-    <p>Or register if you dont have a account yet!</p>
-    <a href="register.php">Register</a>
-</div> 
+<div id="splash"><center><img src="img/1.gif" /></center></div>
 </body>
 </html>
