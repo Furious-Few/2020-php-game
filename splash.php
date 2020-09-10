bd892b4ff13a3abe0a7c3c80d65f8a2bd8a1c64c
Ravithiele <63392140+Ravithiele@users.noreply.github.com>
2020-09-10 09:40:47 +0200

    huts

--- splash.php
+++
@@ -1,4 +0,0 @@
-<?php session_start();
-
-$(document).ready(function () { if( $.cookie('splashscreen') == null ) { // Here you are checking if cookie is existing if not you are showing a splash screen and set a cookie $("#splashscreen").fadeIn(); $.cookie("splashscreen", 1, { expires : 10 }); // cookie is valid for 10 days } $("#splashscreen").click(function () { $("#splashscreen").fadeOut(2000); }); });
-
