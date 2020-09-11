db07820c0fc3e9843f31a3387aa79bb21e23b121
DevCrew02 <63283868+DevCrew02@users.noreply.github.com>
2020-09-11 08:41:26 +0200

    c'est la vie

--- dashboard.php
+++ dashboard.php
@@ -49,6 +49,9 @@ jQuery(document).ready(function() {
 </head>
 <body>
     <header>
+	<audio src="/music/starwars.mp3" controls>
+	<p>If you are reading this, it is because your browser does not support the audio element.</p>
+	</audio>
         <nav>
             <div class="leaderbord">
             <h2>Leaderboard</h2>
