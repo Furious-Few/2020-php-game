690a8bad8faf2a68be3ca59e5869e00341e2d42b
DevCrew02 <63283868+DevCrew02@users.noreply.github.com>
2020-09-15 10:00:12 +0200

    timer

---
+++ sql/countdowntimer.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 15, 2020 at 09:53 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `countdowntimer`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `timer`
+--
+
+CREATE TABLE `timer` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `h` int(50) NOT NULL,
+  `m` int(50) NOT NULL,
+  `s` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
