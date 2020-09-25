ed06aeb698269497fcc9fcd35f0476d03677d2df
DevCrew02 <63283868+DevCrew02@users.noreply.github.com>
2020-09-25 09:16:23 +0200

    Update planets.php

--- planets.php
+++ planets.php
@@ -2,7 +2,7 @@
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title>Brought to you by Huts "Space Miner"</title>
+  <title>Brought to you by Huts: Space Miner</title>
   <link rel="stylesheet" href="css/planet-style.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
 <header> 
