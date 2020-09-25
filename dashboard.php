764a6f291ee6cd2846fcb251b676705b7a6a15da
Rupserdub <smitruben@home.nl>
2020-09-25 10:30:26 +0200

    lil ni

--- dashboard.php
+++ dashboard.php
@@ -27,22 +27,22 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
     <link rel="stylesheet" href="css/dashboard-style.css">
     <title>Space miner</title>
     <script src="http://code.jquery.com/jquery-latest.js"></script>
-<script>
-    $(document).ready(function(){
-		 $("#div_refresh").load("load.php");
-        setInterval(function() {
+    <script>
+        $(document).ready(function(){
+         $("#div_refresh").load("load.php");
+         setInterval(function() {
             $("#div_refresh").load("load.php");
         }, 100);
+     });
+
+ </script>
+ <!-- script for scrolling down -->
+ <script type="text/javascript">
+    jQuery(document).ready(function() {
+
+        jQuery('body').animate({scrollTop: +10000}, 1000);
+
     });
- 
-</script>
-<!-- script for scrolling down -->
-<script type="text/javascript">
-jQuery(document).ready(function() {
-    
-    jQuery('body').animate({scrollTop: +10000}, 1000);
-    
-});
 </script>
 <!-- end of script -->
 </head>
@@ -51,25 +51,25 @@ jQuery(document).ready(function() {
     <header>
         <nav>
             <div class="leaderbord">
-            <h2>Leaderboard</h2>
+                <h2>Leaderboard</h2>
                 <?php
-                    $sql = "SELECT * FROM users ORDER BY coins DESC LIMIT 5";
-                    $query = $conn->query($sql);
-                    $users = $query->fetchAll(PDO::FETCH_ASSOC);    
-        
-                    foreach ($users as $user) {
-                        echo "<li>${user['username']} with ${user['coins']} coins";
-                    }
+                $sql = "SELECT * FROM users ORDER BY coins DESC LIMIT 5";
+                $query = $conn->query($sql);
+                $users = $query->fetchAll(PDO::FETCH_ASSOC);    
+
+                foreach ($users as $user) {
+                    echo "<li>${user['username']} with ${user['coins']} coins";
+                }
                 ?>
             </div>
-        
-   
+
+
             <div class="message">
                 <h3>Log:</h3>
                 <?php
-                    if(isset($_GET['msg'])){
+                if(isset($_GET['msg'])){
                     echo $_GET['msg'];
-                    }
+                }
                 ?>
             </div>
             <div class="nav-resources">
@@ -83,6 +83,49 @@ jQuery(document).ready(function() {
         <hr>
     </header>
     <main>
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+
+        <div class="main-flex">
+            <div class="meteor">
+                <a href="backend/meteorClick.php">
+                    <button onclick="move()"><img src="img/meteor.png" alt="meteor-image" width="150px" height="300px"></button>
+                    <div id="myProgress">
+                      <div id="myBar"></div>
+                  </div>
+                  <button onclick="move()">Click Me and wait 3 seconds!</button> 
+              </a>
+          </div>
+          <script>
+            var i = 0;
+            function move() {
+              if (i == 0) {
+                i = 1;
+                var elem = document.getElementById("myBar");
+                var width = 1;
+                var id = setInterval(frame, 30);
+                function frame() {
+                  if (width >= 100) {
+                    clearInterval(id);
+                    i = 0;
+                } else {
+                    width++;
+                    elem.style.width = width + "%";
+                }
+            }
+        }
+    }
+
+
+</script>
+<div class="universe">
+    <a href="planets.php">
+        <img src="img/universe.png" alt="universe" width="300px" height="300px">
+    </a>
+</div>
+=======
+>>>>>>> Stashed changes
 	<audio src="starwars.mp3" controls></audio>
         <div class="main-flex">
             <div class="meteor">
@@ -108,33 +151,70 @@ jQuery(document).ready(function() {
                     <p>Sell 1 iron, worth 10 coins</p>
                     <input type="submit" value="Sell!">
                 </form>
+>>>>>>> a75c4ce6bfa55f0f8b28b7b6ecc0561ecff37267
 
-                <!-- form for selling 1 gold -->
-                <form action="backend/shop.php" method="post">
-                    <input type="hidden" name="formType" value="gold"/>
-                    <p>Sell 1 gold, worth 50 coins</p>
-                    <input type="submit" value="Sell!">
-                </form>
+<div class="shop">
+    <!-- form for selling 1 iron -->
+    <form action="backend/shop.php" method="post">
+        <input type="hidden" name="formType" value="iron"/>
+        <p>Sell 1 iron, worth 10 coins</p>
+        <input type="submit" value="Sell!">
+    </form>
 
-                <!-- form for selling 1 diamond -->
-                <form action="backend/shop.php" method="post">
-                    <input type="hidden" name="formType" value="diamond"/>
-                    <p>Sell 1 diamond, worth 200 coins</p>
-                    <input type="submit" value="Sell!">
-                </form>
+    <!-- form for selling 1 gold -->
+    <form action="backend/shop.php" method="post">
+        <input type="hidden" name="formType" value="gold"/>
+        <p>Sell 1 gold, worth 50 coins</p>
+        <input type="submit" value="Sell!">
+    </form>
 
-                <form action="backend/sell_all.php" method="post">
-                    <input type="hidden" name="formType" value="iron">
-                    <label for="Iron">Sell all Iron</label><br>
-                    <input type="submit" value="Sell all!">
-                </form>
+    <!-- form for selling 1 diamond -->
+    <form action="backend/shop.php" method="post">
+        <input type="hidden" name="formType" value="diamond"/>
+        <p>Sell 1 diamond, worth 200 coins</p>
+        <input type="submit" value="Sell!">
+    </form>
 
-                <form action="backend/sell_all.php" method="post">
-                    <input type="hidden" name="formType" value="gold"/>
-                    <label for="Gold">Sell all Gold</label>
-                    <input type="submit" value="Sell all!">
-                </form>
+    <form action="backend/sell_all.php" method="post">
+        <input type="hidden" name="formType" value="iron">
+        <label for="Iron">Sell all Iron</label><br>
+        <input type="submit" value="Sell all!">
+    </form>
 
+<<<<<<< HEAD
+    <form action="backend/sell_all.php" method="post">
+        <input type="hidden" name="formType" value="gold"/>
+        <label for="Gold">Sell all Gold</label>
+        <input type="submit" value="Sell all!">
+    </form>
+
+    <form action="backend/sell_all.php" method="post">
+        <input type="hidden" name="formType" value="diamond"/>
+        <label for="diamond">Sell all diamonds</label>
+        <input type="submit" value="Sell all!">
+    </form>
+</div>
+
+<div class="hangar">
+    <a href="hangar.php">
+        <img src="img/dashboard-hangar.gif" alt="Hangar">
+    </a>
+</div>
+</div> 
+</main>
+<div class="chat-name">
+    <h1>Chat</h1>
+</div>
+
+<hr>
+<footer>
+    <div class="footer-flex">
+        <div class="chat">
+
+            <div class="chat-flex">
+
+                <div id="div_refresh">
+=======
                 <form action="backend/sell_all.php" method="post">
                     <input type="hidden" name="formType" value="diamond"/>
                     <label for="diamond">Sell all diamonds</label>
@@ -165,13 +245,14 @@ jQuery(document).ready(function() {
                 <div class="chat-flex">
                 
                     <div id="div_refresh">
+>>>>>>> a75c4ce6bfa55f0f8b28b7b6ecc0561ecff37267
 
-                    </div>
                 </div>
+            </div>
             
-                
+
             <div class="form">
-                
+
                 <form action="backend/chatcontroller.php" method="post">
                     <input type="hidden" name="formType" value="chat">
                     <br>
@@ -180,33 +261,33 @@ jQuery(document).ready(function() {
                     <!-- <input type="submit" value="send" required> -->
                 </form>
             </div>
-            </div>
-            <!-- start of bank history -->
-            <div class="bank-overkoepelend">
-                <h1>bank history</h1>
-                <hr>
+        </div>
+        <!-- start of bank history -->
+        <div class="bank-overkoepelend">
+            <h1>bank history</h1>
+            <hr>
             <div class="bank-history">
-                
-                    <?php
 
-                        $sql = "SELECT * FROM bank_history";
-                        $query = $conn->query($sql);
-                        $bank_historys = $query->fetchAll(PDO::FETCH_ASSOC);
+                <?php
+
+                $sql = "SELECT * FROM bank_history";
+                $query = $conn->query($sql);
+                $bank_historys = $query->fetchAll(PDO::FETCH_ASSOC);
 
                         // echo '<h1>Bank history</h1>';
                         // echo '<hr>';
-                        foreach ($bank_historys as $bank) {
-                            
-                            echo "${bank['name']} ${bank['message']}<br><br>";
+                foreach ($bank_historys as $bank) {
 
-                        }
-                    ?>
+                    echo "${bank['name']} ${bank['message']}<br><br>";
+
+                }
+                ?>
             </div>
-            </div>
-            
         </div>
-        
-        
-    </footer>
+
+    </div>
+
+
+</footer>
 </body>
 </html>
