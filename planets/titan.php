0bc896eac37c47e5dd30b814980d2f4f71f7d7f5
DevCrew02 <63283868+DevCrew02@users.noreply.github.com>
2020-10-09 09:51:54 +0200

    Y

--- planets/titan.php
+++ planets/titan.php
@@ -21,74 +21,152 @@
   <!-- Define all of the tiles: -->
   <div class="box">
     <div class="boxInner">
-      <img src="img/1.jpeg" />
+      <img src="img/1.png" />
       <div class="titleBox">Tegel1</div>
     </div>
   </div>
   <div class="box">
     <div class="boxInner">
-      <img src="img/1.jpeg" />
+      <img src="img/1.png" />
       <div class="titleBox">Tegel2</div>
     </div>
   </div>
   <div class="box">
     <div class="boxInner">
-      <img src="img/1.jpeg" />
+      <img src="img/1.png" />
       <div class="titleBox">Tegel3</div>
     </div>
   </div>
   <div class="box">
     <div class="boxInner">
-      <img src="img/1.jpeg" />
+      <img src="img/1.png" />
       <div class="titleBox">Tegel4</div>
     </div>
   </div>
   <div class="box">
     <div class="boxInner">
-      <img src="img/1.jpeg" />
+      <img src="img/1.png" />
       <div class="titleBox">Tegel5</div>
     </div>
   </div>
   <div class="box">
     <div class="boxInner">
-      <img src="img/1.jpeg" />
+      <img src="img/1.png" />
       <div class="titleBox">Tegel6</div>
     </div>
   </div>
   <div class="box">
     <div class="boxInner">
-      <img src="img/1.jpeg" />
+      <img src="img/1.png" />
       <div class="titleBox">Tegel7</div>
     </div>
   </div>
   <div class="box">
     <div class="boxInner">
-      <img src="img/1.jpeg" />
+      <img src="img/1.png" />
       <div class="titleBox">Tegel8</div>
     </div>
   </div>
   <div class="box">
     <div class="boxInner">
-      <img src="img/1.jpeg" />
+      <img src="img/1.png" />
       <div class="titleBox">Tegel9</div>
     </div>
   </div>
   <div class="box">
     <div class="boxInner">
-      <img src="img/1.jpeg" />
+      <img src="img/1.png" />
       <div class="titleBox">Tegel10</div>
     </div>
   </div>
   <div class="box">
     <div class="boxInner">
-      <img src="img/1.jpeg" />
+      <img src="img/1.png" />
       <div class="titleBox">Tegel11</div>
     </div>
   </div>
   <div class="box">
     <div class="boxInner">
-      <img src="img/1.jpeg" />
+      <img src="img/1.png" />
       <div class="titleBox">Tegel12</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.png" />
+      <div class="titleBox">Tegel1</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.png" />
+      <div class="titleBox">Tegel2</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.png" />
+      <div class="titleBox">Tegel3</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.png" />
+      <div class="titleBox">Tegel4</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.png" />
+      <div class="titleBox">Tegel5</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.png" />
+      <div class="titleBox">Tegel6</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.png" />
+      <div class="titleBox">Tegel7</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.png" />
+      <div class="titleBox">Tegel8</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.png" />
+      <div class="titleBox">Tegel9</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.png" />
+      <div class="titleBox">Tegel10</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.png" />
+      <div class="titleBox">Tegel11</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.png" />
+      <div class="titleBox">Tegel12</div>
+    </div>
+  </div>
+    <div class="box">
+    <div class="boxInner">
+      <img src="img/1.png" />
+      <div class="titleBox">Tegel25</div>
     </div>
   </div>
   
