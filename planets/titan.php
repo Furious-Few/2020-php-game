2bfc2337cd52f62d5153d113dac537b9164c824b
DevCrew02 <63283868+DevCrew02@users.noreply.github.com>
2020-10-09 09:46:59 +0200

    tietan ;)
    especially for you my friend

--- planets/titan.php
+++ planets/titan.php
@@ -1,1 +1,99 @@
 <?php
+
+?>
+
+
+<!DOCTYPE HTML> 
+<html lang="en-US"> 
+<head> 
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">   
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="planets-style.css">
+  <title>Tiles</title>
+  
+  <style type="text/css">
+
+  </style> 
+</head> 
+<body> 
+<div class="wrap">
+    
+  <!-- Define all of the tiles: -->
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.jpeg" />
+      <div class="titleBox">Tegel1</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.jpeg" />
+      <div class="titleBox">Tegel2</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.jpeg" />
+      <div class="titleBox">Tegel3</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.jpeg" />
+      <div class="titleBox">Tegel4</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.jpeg" />
+      <div class="titleBox">Tegel5</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.jpeg" />
+      <div class="titleBox">Tegel6</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.jpeg" />
+      <div class="titleBox">Tegel7</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.jpeg" />
+      <div class="titleBox">Tegel8</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.jpeg" />
+      <div class="titleBox">Tegel9</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.jpeg" />
+      <div class="titleBox">Tegel10</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.jpeg" />
+      <div class="titleBox">Tegel11</div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="boxInner">
+      <img src="img/1.jpeg" />
+      <div class="titleBox">Tegel12</div>
+    </div>
+  </div>
+  
+</div>
+<!-- /#wrap -->	
+
+</body> 
+</html>
